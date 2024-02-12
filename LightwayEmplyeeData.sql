3311070bdd7aeae124de4ddf1e74e9e1c6e77c9c
salamitosin <81862571+salamitosin@users.noreply.github.com>
2024-02-12 20:39:39 +0100

    Add files via upload
    Covid 19 Advance Data Exploration 
    
    Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types

---
+++ LightwayEmplyeeData.sql
@@ -0,0 +1,95 @@
+--Simple Example
+
+Create Table EmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+Create Table EmployeeSalary 
+(EmployeeID int, 
+JobTitle varchar(50), 
+Salary int
+)
+
+Insert into EmployeeDemographics VALUES
+(1001, 'Jim', 'Halpert', 30, 'Male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31, 'Male')
+
+
+Insert Into EmployeeSalary VALUES
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000)
+
+Insert into EmployeeDemographics VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL, 'Holly', 'Flax', NULL, NULL),
+(1013, 'Darryl', 'Philbin', NULL, 'Male')
+
+select * from EmployeeDemographics
+select * from EmployeeSalary
+
+select EmployeeDemographics.EmployeeID, FirstName, LastName, JobTitle,Salary
+from EmployeeDemographics 
+left outer join EmployeeSalary
+on EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+where FirstName <> 'michael'
+order by Salary desc
+
+select JobTitle, Salary
+from EmployeeDemographics 
+inner join EmployeeSalary
+on EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+where JobTitle = 'salesman'
+
+select JobTitle, avg(Salary) as averageOfSalesman
+from EmployeeDemographics 
+inner join EmployeeSalary
+on EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+where JobTitle = 'salesman'
+group by JobTitle
+
+Create Table WareHouseEmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+
+Insert into WareHouseEmployeeDemographics VALUES
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+(1052, 'Val', 'Johnson', 31, 'Female')
+
+select * from LightwayEmploymentDemographyData.dbo.WareHouseEmployeeDemographics
+
+select * from LightwayEmploymentDemographyData.dbo.EmployeeDemographics
+
+select * from LightwayEmploymentDemographyData.dbo.WareHouseEmployeeDemographics
+union
+select * from LightwayEmploymentDemographyData.dbo.EmployeeDemographics
+
+select * from LightwayEmploymentDemographyData.dbo.WareHouseEmployeeDemographics
+union all
+select * from LightwayEmploymentDemographyData.dbo.EmployeeDemographics
+order by EmployeeID
+
+
+
